ed168705ef0faf2b1d214745cc0f1c962d6bc750
ali <aghorbani778@gmail.com>
2023-03-29 20:53:05 +0430

    some change in models name and forign key

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class CarClass(models.Model):
+    title = models.CharField(max_length=100,verbose_name='title')
+    description = models.TextField(blank=True, verbose_name='description')
+
+
+class CarClassSUb(models.Model):
+    car_sub = models.ForeignKey(CarClass, max_length=100, related_name='car', on_delete=models.CASCADE, verbose_name='title')
+    description = models.TextField(verbose_name='description')
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+from .models import CarClass
+
+
+class MainPage(generic.ListView):
+    model = CarClass
+    template_name = 'pages/car_show.html'
+    context_object_name = 'car'
+
+
+
