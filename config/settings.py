d10be1718a3b6d3cfe91b77a77043e87559e91fe
ali <aghorbani778@gmail.com>
2023-04-20 20:48:01 +0430

    some change in style of html file

--- config/settings.py
+++ config/settings.py
@@ -151,8 +151,9 @@ LANGUAGES = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static/'))]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
